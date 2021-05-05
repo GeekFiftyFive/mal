67ef43275c253d4ef5075011b647c8deffe5d403
GeekFiftyFive <op15736@my.bristol.ac.uk>
2021-05-05 19:58:33 +0100

    Pass step 0

--- impls/TypeScript/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exec yarn run $(dirname $0)/${STEP:-stepA_mal}.ts "${@}"
--- impls/TypeScript/step0_repl.ts
+++ impls/TypeScript/step0_repl.ts
@@ -1,5 +1,6 @@
 import * as readline from 'readline';
 
+process.stdin.setEncoding('utf-8');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -18,19 +19,13 @@ const PRINT = (arg1: string) => {
 };
 
 const rep = (arg1: string) => {
-    PRINT(EVAL(READ(arg1)));
+    return PRINT(EVAL(READ(arg1)));
 };
 
-const handler = () => {
-    const exit = false;
-
-    while(!exit) {
-        rl.question('user> ', (answer) => {
-            console.log(rep(answer));
-        });
-    }
-
-    rl.close();
-};
-
-handler();
+rl.setPrompt('user> ');
+rl.prompt();
+rl.on('line', (input: string) => {
+    console.log(rep(input));
+    rl.prompt();
+});
+rl.on('close', process.exit);
