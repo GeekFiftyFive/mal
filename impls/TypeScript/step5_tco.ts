05dc44da339a3ef5e98009df08becacc95184b9a
GeekFiftyFive <op15736@my.bristol.ac.uk>
2021-05-10 20:29:31 +0100

    Refactor to eliminate duplicate default case

--- impls/TypeScript/step5_tco.ts
+++ impls/TypeScript/step5_tco.ts
@@ -63,90 +63,77 @@ const EVAL = (ast: MalType, env: Env): MalType => {
                 return ast;
             }
             const first = ast.getList()[0];
-            if (first instanceof MalSymbol) {
-                switch(first.get()) {
-                    case lisp_def:
-                        const key = ast.getList()[1] as MalSymbol;
-                        const val = EVAL(ast.getList()[2], env);
-                        env.set(key.get(), val);
-                        return val;
-                    case lisp_let:
-                        const letEnv = new Env(env);
-                        const bindings = ast.getList()[1] as MalList;
-                        const grouped = group_array(bindings.getList() as MalType[]);
-                        grouped.forEach(([key, val]) => {
-                            let name: string;
-                            if (key instanceof MalSymbol) {
-                                name = key.get();
-                            } else {
-                                throw new Error('even value in pair must be symbol');
-                            }
-                            letEnv.set(name, EVAL(val, letEnv));
-                        });
-                        env = letEnv;
-                        ast = ast.getList()[2];
-                        break;
-                    case lisp_do:
-                        const do_list = new MalList();
-                        do_list.add(...(ast.getList().slice(1, -2)));
-                        eval_ast(do_list, env) as MalList;
-                        ast = ast.getList()[ast.getList().length - 1];
-                        break;
-                    case lisp_if:
-                        const condition = EVAL(ast.getList()[1], env);
-                        if (condition instanceof MalNil || (condition instanceof MalBoolean && !condition.is())) {
-                            if (ast.getList().length < 4) {
-                                return MalNil.singleton;
-                            }
-    
-                            ast = ast.getList()[3];
+            let form: string;
+            if(first instanceof MalSymbol) {
+                form = first.get();
+            }
+            switch(form) {
+                case lisp_def:
+                    const key = ast.getList()[1] as MalSymbol;
+                    const val = EVAL(ast.getList()[2], env);
+                    env.set(key.get(), val);
+                    return val;
+                case lisp_let:
+                    const letEnv = new Env(env);
+                    const bindings = ast.getList()[1] as MalList;
+                    const grouped = group_array(bindings.getList() as MalType[]);
+                    grouped.forEach(([key, val]) => {
+                        let name: string;
+                        if (key instanceof MalSymbol) {
+                            name = key.get();
                         } else {
-                            ast = ast.getList()[2];
+                            throw new Error('even value in pair must be symbol');
                         }
-                        break;
-                    case lisp_fn:
-                        const fn = new MalFunction((...exprs: MalType[]) => {
-                            const binds = ((ast as MalList).getList()[1] as MalList).getList() as MalSymbol[];
-                            const fnEnv = new Env(env, binds, exprs);
-                            return EVAL((ast as MalList)[2], fnEnv); 
-                        });
-                        return new MalClosure(
-                            (ast as MalList).getList()[2],
-                            ((ast as MalList).getList()[1] as MalList).getList() as MalSymbol[],
-                            env,
-                            fn
-                        );
-                    default:
-                        const evaluated = eval_ast(ast, env) as MalList;
-                        const op = evaluated.getList()[0];
-                        const args = evaluated.getList().slice(1);
+                        letEnv.set(name, EVAL(val, letEnv));
+                    });
+                    env = letEnv;
+                    ast = ast.getList()[2];
+                    break;
+                case lisp_do:
+                    const do_list = new MalList();
+                    do_list.add(...(ast.getList().slice(1, -2)));
+                    eval_ast(do_list, env) as MalList;
+                    ast = ast.getList()[ast.getList().length - 1];
+                    break;
+                case lisp_if:
+                    const condition = EVAL(ast.getList()[1], env);
+                    if (condition instanceof MalNil || (condition instanceof MalBoolean && !condition.is())) {
+                        if (ast.getList().length < 4) {
+                            return MalNil.singleton;
+                        }
 
-                        if (op instanceof MalFunction) {
-                            return op.exec(...(args.map((arg) => {
-                                return eval_ast(arg, env);
-                            })));
-                        } else if (op instanceof MalClosure) {
-                            ast = op.getAst();
-                            env = new Env(env, op.getParams(), args);
-                        } else {
-                            throw new Error('op is of unknown type');
-                        }
-                }
-            } else {
-                // TODO: Refactor
-                const evaluated = eval_ast(ast, env) as MalList;
-                const op = evaluated.getList()[0];
-                const args = evaluated.getList().slice(1)
-                if (op instanceof MalFunction) {
-                    return op.exec(...(args.map((arg) => {
-                        return eval_ast(arg, env);
-                    })));
-                } else if (op instanceof MalClosure) {
-                    ast = op.getAst();
-                    env = new Env(env, op.getParams(), args);
-                } else {
-                    throw new Error('op is of unknown type');
-                }
+                        ast = ast.getList()[3];
+                    } else {
+                        ast = ast.getList()[2];
+                    }
+                    break;
+                case lisp_fn:
+                    const fn = new MalFunction((...exprs: MalType[]) => {
+                        const binds = ((ast as MalList).getList()[1] as MalList).getList() as MalSymbol[];
+                        const fnEnv = new Env(env, binds, exprs);
+                        return EVAL((ast as MalList)[2], fnEnv); 
+                    });
+                    return new MalClosure(
+                        (ast as MalList).getList()[2],
+                        ((ast as MalList).getList()[1] as MalList).getList() as MalSymbol[],
+                        env,
+                        fn
+                    );
+                default:
+                    const evaluated = eval_ast(ast, env) as MalList;
+                    const op = evaluated.getList()[0];
+                    const args = evaluated.getList().slice(1);
+
+                    if (op instanceof MalFunction) {
+                        return op.exec(...(args.map((arg) => {
+                            return eval_ast(arg, env);
+                        })));
+                    } else if (op instanceof MalClosure) {
+                        ast = op.getAst();
+                        env = new Env(env, op.getParams(), args);
+                    } else {
+                        throw new Error('op is of unknown type');
+                    }
             }
         } else {
             return eval_ast(ast, env);
